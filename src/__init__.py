e8f056f8f18c67251116367fc02fe2885e30f568
Nirav L. Lekinwala <nirav.lekinwala@iitb.ac.in>
2022-05-26 16:16:07 +0530

    Minor Changes

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+from .pyGNMF import internal_functions
+from .pyGNMF import covariance_matrix_handling
+from .pyGNMF import gnmf_projgrad_with_cov
+from .pyGNMF import gnmf_multupd_with_cov
+from .pyGNMF import nmf_multupd
+
