aa118658b5a04812eea923245045850dc5f16338
Nirav L. Lekinwala <nirav.lekinwala@iitb.ac.in>
2022-05-30 13:18:42 +0530

    Minor Changes to Test Example

--- test/test1_GNMF_With_Full_Covariance.py
+++ test/test1_GNMF_With_Full_Covariance.py
@@ -12,27 +12,26 @@ from pyGNMF import gnmf_projected_gradient as gproj
 
 data = io.loadmat("pyGNMF_testDataset.mat")
 X_matrix = np.array(data['conc_with_error'])
-#X_matrix = np.array(np.random.rand(20, 8))
 covariance = np.array(data['covariance'])
-#covariance = np.array(np.eye(X_matrix.shape[0]*X_matrix.shape[1]))
-#G_init = np.array(data['g_init'])
-#F_init = np.array(data['f_init'])
+G_init = np.array(data['g_init'])
+F_init = np.array(data['f_init'])
 GMat, FMat, OFunc = gproj.running_method(
     X_matrix,
     covariance,
-    G_init = 'random',
-    F_init = 'random',
+    G_init = G_init[0],
+    F_init = F_init[0],
     option='row_stacked',
     num_fact=7,
     num_init=1,
-    alpha_init_G=1e-6,
-    alpha_init_F=1e-8,
-    max_iter=10000,
+    alpha_init_G=1,
+    alpha_init_F=1,
+    max_iter=100,
     tolerance=1e-6,
     conv_typ='relative',
     conv_num=3,
 )
 
+"""
 GMat, FMat, OFunc = gmult.running_method(
     X_matrix,
     covariance,
@@ -58,3 +57,4 @@ GMat, FMat, OFunc = mult.running_method(
     conv_typ='relative',
     conv_num=3,
 )
+"""
